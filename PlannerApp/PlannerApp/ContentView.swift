b3b26da200c60f3cd007151a77d067ed676c7f31
Pankaj kumar Rout <pankajer.desk@gmail.com>
2020-10-20 13:37:10 +0530

    removed text

--- PlannerApp/PlannerApp/ContentView.swift
+++ PlannerApp/PlannerApp/ContentView.swift
@@ -24,7 +24,7 @@ struct ContentView: View {
                 
                 VStack{
                     
-                    Text("Hello")
+                    Text("")
                     
                     
                     
